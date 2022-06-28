dd80c6a639b014bb48ccad7dc26ded3669e536be
Deyson19 <deiandresvent@hotmail.com>
2022-06-27 23:37:31 -0500

    mejoras en el funcionamiento y recuperacion de scripts

---
+++ Models/PosicionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiFutbolistas.Models
+{
+    public class PosicionViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+    }
+}
